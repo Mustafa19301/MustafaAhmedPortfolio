9c7589b38cc8aa66bccdb33253933ee376ac4ac5
Mustafa Ahmed <155925871+Mustafa19301@users.noreply.github.com>
2024-09-16 12:23:59 -0400

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from 'react-type-animation';
+import Link from "next/link";
 
 const HeroSection = () => {
     return (
@@ -33,12 +34,16 @@ const HeroSection = () => {
                         An aspiring software developer willing to learn new experience.
                     </p>
                     <div>
-                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-green-500 to-teal-500 hover:bg-slate-200 text-white">
+                        <Link 
+                        href="/#contact"
+                        className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-green-500 to-teal-500 hover:bg-slate-200 text-white">
                             Hire Me
-                        </button>
-                        <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-green-500 to-teal-500 hover:bg-slate-800 text-white border mt-3">
+                        </Link>
+                        <Link
+                        href="https://mustafa19301.github.io/"
+                        className="px-1 inline-block py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-green-500 to-teal-500 hover:bg-slate-800 text-white border mt-3">
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Other Website</span>
-                        </button>
+                        </Link>
                     </div>
                 </div>
                 <div className="col-span-4 place-self-center mt-4 lg:mt-0">
