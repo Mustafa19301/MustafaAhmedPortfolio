193861cde9e64fe0e9a7080f4a8a0dd5a562c586
Mustafa Ahmed <mustafaahmed3848@gmail.com>
2024-09-16 11:19:44 -0400

    first commit

---
+++ src/app/components/EmailSection.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const EmailSection = () => {
+    return (
+        <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
+            <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+            <div className="z-10">
+                <h5 className="text-xl font bold text-white my-2">Let's connect</h5>
+                <p className="text-[#ADB7BE] mb-4 max-w-md">
+                    {" "}
+                    I'm currently looking for new opportunities, my inbox is open so feel free to connect.
+                    I'll do my best to get back to you as soon as possible! Contact email: mustafaahmed3848@gmail.com
+                </p>
+                <div className="socials flex flex-row gap-2">
+                    <Link href="https://github.com/Mustafa19301">
+                        <Image src="/images/GITHUB2.png" width={50} height={50} />
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/mustafa-a-252700297/">
+                        <Image src="/images/LINKEDIN.png" width={50} height={50} />
+                    </Link>
+                </div>
+            </div>
+            <div className="flex flex-col gap-4">
+                <Image src="/images/Program2.png" width={700} height={700} />
+                </div>
+        </section>
+    );
+};
+
+export default EmailSection;
---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,60 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from 'react-type-animation';
+
+const HeroSection = () => {
+    return (
+        <section className="lg:py-16">
+            <div className="grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-green-600">
+                            Hello, I'm {" "}
+                        </span>
+                        <br></br>
+                        <TypeAnimation
+                            sequence={[
+                                'Mustafa Ahmed',
+                                1000,
+                                'Software Developer',
+                                1000,
+                                'Web Developer',
+                                1000,
+                                'CompSci Student',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                        An aspiring software developer willing to learn new experience.
+                    </p>
+                    <div>
+                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-green-500 to-teal-500 hover:bg-slate-200 text-white">
+                            Hire Me
+                        </button>
+                        <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-green-500 to-teal-500 hover:bg-slate-800 text-white border mt-3">
+                            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Other Website</span>
+                        </button>
+                    </div>
+                </div>
+                <div className="col-span-4 place-self-center mt-4 lg:mt-0">
+                    <div className="rounded-full bg-[#181818] w-[200px] h-[200px] lg:w-[400px] lg:h-[400px] relative">
+                        <Image
+                            src="/images/RobotCartoon.png"
+                            alt="Robot Image"
+                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            width={250}
+                            height={250}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HeroSection;
