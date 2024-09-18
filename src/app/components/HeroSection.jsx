5f3cb6a99af945c715310eab31db0dca06be67dc
Mustafa Ahmed <155925871+Mustafa19301@users.noreply.github.com>
2024-09-17 21:22:32 -0400

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -52,8 +52,8 @@ const HeroSection = () => {
                             src="/images/ProfileMustafa.png"
                             alt="Robot Image"
                             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            width={250}
-                            height={250}
+                            width={300}
+                            height={300}
                         />
                     </div>
                 </div>
