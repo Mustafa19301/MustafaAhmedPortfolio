718466b47f6781a2fec6deab09ad249a766e228e
Mustafa Ahmed <155925871+Mustafa19301@users.noreply.github.com>
2024-09-25 16:40:32 -0400

    Update AboutSection.jsx (Added an id about)

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -55,7 +55,7 @@ const AboutSection = () => {
     };
 
     return (
-        <section className="text-white">
+        <section id="about" className="text-white">
             <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
                 <Image src="/images/Program.png" width={700} height={700} alt = "program"/>
                 <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
