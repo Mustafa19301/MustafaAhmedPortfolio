369fd6894e8bc6b990a554ef8144930a8d1493b6
Mustafa Ahmed <155925871+Mustafa19301@users.noreply.github.com>
2024-09-25 16:39:25 -0400

    Update Navbar.jsx (Typo error)

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -35,14 +35,14 @@ const Navbar = () => {
                 <div className="mobile-menu block md:hidden">
                     {!navbarOpen ? (
                         <button
-                            onCLick={() => setNavbarOpen(true)}
+                            onClick={() => setNavbarOpen(true)}
                             className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
                         >
                             <Bars3Icon className="h-5 w-5" />
                         </button>
                     ) : (
                         <button
-                            onCLick={() => setNavbarOpen(false)}
+                            onClick={() => setNavbarOpen(false)}
                             className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
                         >
                             <XMarkIcon className="h-5 w-5" />
