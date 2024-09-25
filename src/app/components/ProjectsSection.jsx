28cace9afa8ab348a56ac479b6715f55b7c4c11d
Mustafa Ahmed <155925871+Mustafa19301@users.noreply.github.com>
2024-09-25 16:41:27 -0400

    Update ProjectsSection.jsx (Added an id projects)

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -46,7 +46,7 @@ const projectsData = [
 
 const ProjectsSection = () => {
     return (
-        <>
+        <section id="projects">
         <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
             My Projects
         </h2>
@@ -60,7 +60,7 @@ const ProjectsSection = () => {
             />
             ))}
             </div>
-        </>
+        </section>
     );
 };
 
