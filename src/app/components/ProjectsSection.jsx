d46179a58ec83d11963a8c5d07b680f799f217cd
Mustafa Ahmed <155925871+Mustafa19301@users.noreply.github.com>
2024-12-11 20:11:49 -0500

    Updated the project card and links

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import ProjectCard from "./ProjectCard";
 
 const projectsData = [
-    {
+  {
     id: 1,
+    title: "SR Dashboard Heatmap",
+    description: "Group project that we developed using React and Object-Oriented Programming Python using data from City of Windsor (All-service request)",
+    image: "/images/projects/dashboardheatmap.png",
+    tag: ["All", "Mobile"],
+    gitUrl: "https://github.com/Fakkiie/COMP3220-Team2",
+  },
+  {
+    id: 2,
     title: "Full-Stack Portfolio Web Application",
     description: "Developed, Designed and Deployed a full stack portfolio web application with React",
     image: "/images/projects/6.PNG",
@@ -11,7 +19,7 @@ const projectsData = [
     gitUrl: "https://github.com/Mustafa19301",
   },
   {
-    id: 2,
+    id: 3,
     title: "Simple HTML Portfolio Website",
     description: "Built an HTML simple portfolio website previously for projects",
     image: "/images/projects/1.PNG",
@@ -19,15 +27,15 @@ const projectsData = [
     gitUrl: "https://github.com/Mustafa19301",
   },
   {
-    id: 3,
+    id: 4,
     title: "RPG Adventure JAVA Game",
     description: "Programmed an RPG game using JAVA that plays a knight programmed to defeat mobs and earn rewards",
     image: "/images/projects/2.PNG",
     tag: ["All", "Web"],
-    gitUrl: "https://github.com/Mustafa19301",
+    gitUrl: "https://github.com/Mustafa19301/JAVA-RPG-action-project",
   },
   {
-    id: 4,
+    id: 5,
     title: "Website Designed Application",
     description: "Designed professional medical website for a leading company primarily focused on podiatry medicine.",
     image: "/images/projects/3.png",
@@ -35,12 +43,12 @@ const projectsData = [
     gitUrl: "https://www.ahmedfootsurgery.com/",
   },
   {
-    id: 5,
+    id: 6,
     title: "Tic-Tac-Toe Python Game",
     description: "Programmed a Tic-Tac-Toe game using Python that utilizes Definitons",
     image: "/images/projects/4.png",
     tag: ["All", "Mobile"],
-    gitUrl: "https://github.com/Mustafa19301",
+    gitUrl: "https://github.com/Mustafa19301/Python-Tic-Tac-Toe",
   },
   ];
 
