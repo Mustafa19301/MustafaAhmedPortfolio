cc4a25443f0b5de7f7bea3fbd765bb4b39214433
Mustafa Ahmed <155925871+Mustafa19301@users.noreply.github.com>
2024-12-30 22:03:44 -0500

    Update 1 project card

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -4,6 +4,14 @@ import ProjectCard from "./ProjectCard";
 const projectsData = [
   {
     id: 1,
+    title: "Live Face and Object Detection",
+    description: "Developed a Live Face and Object Detection using Python that integrates OpenCV and YOLO5 models",
+    image: "/images/projects/FaceDetection.PNG",
+    tag: ["All", "Mobile"],
+    gitUrl: "https://github.com/Mustafa19301/Live-Face-and-Object-Detection",
+  },
+  {
+    id: 2,
     title: "SR Dashboard Heatmap",
     description: "Group project that we developed using React and Object-Oriented Programming Python using data from City of Windsor (All-service request)",
     image: "/images/projects/dashboardheatmap.PNG",
@@ -11,7 +19,7 @@ const projectsData = [
     gitUrl: "https://github.com/Fakkiie/COMP3220-Team2",
   },
   {
-    id: 2,
+    id: 3,
     title: "Next.js Portfolio Web Application",
     description: "Developed, designed, and deployed a Next.js portfolio web application with robust React framework showcasing a GitHub repository of technical projects and codes",
     image: "/images/projects/6.PNG",
@@ -19,7 +27,7 @@ const projectsData = [
     gitUrl: "https://github.com/Mustafa19301/MustafaAhmedPortfolio",
   },
   {
-    id: 3,
+    id: 4,
     title: "Simple HTML Portfolio Website",
     description: "Built an HTML simple portfolio website previously for projects",
     image: "/images/projects/1.PNG",
@@ -27,7 +35,7 @@ const projectsData = [
     gitUrl: "https://github.com/Mustafa19301",
   },
   {
-    id: 4,
+    id: 5,
     title: "RPG Adventure JAVA Game",
     description: "Programmed an RPG game using JAVA that plays a knight programmed to defeat mobs and earn rewards",
     image: "/images/projects/2.PNG",
@@ -35,7 +43,7 @@ const projectsData = [
     gitUrl: "https://github.com/Mustafa19301/JAVA-RPG-action-project",
   },
   {
-    id: 5,
+    id: 6,
     title: "Website Designed Application",
     description: "Designed professional medical website for a leading company primarily focused on podiatry medicine.",
     image: "/images/projects/welcome.PNG",
@@ -43,7 +51,7 @@ const projectsData = [
     gitUrl: "https://www.ahmedfootsurgery.com/",
   },
   {
-    id: 6,
+    id: 7,
     title: "Tic-Tac-Toe Python Game",
     description: "Programmed a Tic-Tac-Toe game using Python that utilizes Definitons",
     image: "/images/projects/4.png",
